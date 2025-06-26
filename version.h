2f3617ab7a460a99dec73b0a6fc38a248e57bf24
BlueChip <csbluechip@gmail.com>
2025-06-26 23:11:46 +0100

    add version header

---
+++ version.h
@@ -0,0 +1,21 @@
+#ifndef  VERSION_H_
+#define  VERSION_H_
+
+// 0.7.0 - first release .. AI incomplete
+
+#define  TOOLNAME  "TicTac"
+
+#define  VER_MAJ   0
+#define  VER_MIN   7
+#define  VER_SUB   0
+
+#define  STR_(x)   #x
+#define  STR(x)    STR_(x)
+
+#define  VER_STR   "Version " STR(VER_MAJ) "." STR(VER_MIN) "." STR(VER_SUB)
+
+#define  AUTHOR    "\e[0;1;32mBlue\e[0;1;34mChip\e[0m"
+#define  DATE      "June/2025"
+
+#endif //VERSION_H_
+
