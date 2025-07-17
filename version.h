839dd15957c8a9c73f8d7c14e2749d1ca8b679e2
BlueChip <csbluechip@gmail.com>
2025-07-17 03:56:04 +0100

    version bump

--- version.h
+++ version.h
@@ -5,12 +5,13 @@
 // 0.8.0 - full screen selection (with mouse)
 // 0.9.0 - now with basic AI
 // 0.9.5 - The bots are getting there slowly...
+// 0.9.6 - Various interface tweaks, incl "-?" for help
 
 #define  TOOLNAMES  "TicTac"
 
 #define  VER_MAJ    0
 #define  VER_MIN    9
-#define  VER_SUB    4
+#define  VER_SUB    6
 
 #define  STR_(x)    #x
 #define  STR(x)     STR_(x)
@@ -18,7 +19,7 @@
 #define  VER_STR    "Version " STR(VER_MAJ) "." STR(VER_MIN) "." STR(VER_SUB)
 
 #define  AUTHOR     "\e[0;1;32mBlue\e[0;1;34mChip\e[0m"
-#define  DATE       "June/2025"
+#define  DATE       "July/2025"
 
 #endif //VERSION_H_
 
